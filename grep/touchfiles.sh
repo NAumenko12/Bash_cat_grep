0ad90dc5bed0c751aef51735df7c152714878881
NAumenko12 <143539153+NAumenko12@users.noreply.github.com>
2024-12-12 02:07:57 +0700

    Add files via upload

---
+++ grep/touchfiles.sh
@@ -0,0 +1,34 @@
+filename1="test_file_1.txt"
+content1="Little leaves fall softly down
+Red and yellow, orange and brown
+Whirling, twirling round and round
+Falling softly to the ground.
+1
+1
+1
+1
+
+
+Little leaves fall softly down
+To make a carpet on the ground.
+Then, swish, the wind comes whistling by
+And sends them dancing to the sky."
+
+filename2="test_file_2.txt"
+content2="    LITTLE RED HOOD
+And Little little LITTLE wolf
+Eyes are red
+Heart are little
+Feet are delicious
+Somebody check my mental health
+Bim bam bom bzzzzzzzzzz brr bam bam bam
+All day in my head brrrrr ba ba bab aba abbaba
+And little red
+Maybe once more Red
+Maybe once more little
+Maybe once more Red Little together"
+
+
+
+echo "$content1" > "$filename1"
+echo "$content2" > "$filename2"
