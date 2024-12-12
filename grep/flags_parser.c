b889cd4d77c03578b66776d57d744313d101dd2c
NAumenko12 <143539153+NAumenko12@users.noreply.github.com>
2024-12-12 09:35:26 +0700

    Update flags_parser.c

--- grep/flags_parser.c
+++ grep/flags_parser.c
@@ -1,49 +1,88 @@
-#include "s21_cat.h"
+#include "s21_grep.h"
 
-Flags parse_cat_flags(int argc, char **argv, int *error_flag) {
+void pattern_add(Flags *flags, char *pattern) {
+  int n = strlen(pattern);
+  if (flags->len_pattern + n + 2 >= MAX_PATTERN_SIZE) {
+    fprintf(stderr, "Pattern buffer overflow. Increase MAX_PATTERN_SIZE.\n");
+  }
+  if (flags->len_pattern != 0) {
+    strcat(flags->pattern + flags->len_pattern, "|");
+    flags->len_pattern++;
+  }
+  flags->len_pattern +=
+      sprintf(flags->pattern + flags->len_pattern, "(%s)", pattern);
+}
+
+void add_reg_from_file(Flags *flags, char *filepath) {
+  FILE *f = fopen(filepath, "r");
+  if (f == NULL) {
+    if (!flags->s) {
+      perror(filepath);
+    }
+    return;
+  }
+  char line[MAX_PATTERN_SIZE];
+  while (fgets(line, sizeof(line), f)) {
+    if (line[strlen(line) - 1] == '\n') {
+      line[strlen(line) - 1] = '\0';
+    }
+    pattern_add(flags, line);
+  }
+  fclose(f);
+}
+
+Flags Flags_parser(int argc, char *argv[]) {
   Flags flags = {0};
-  struct option long_options[] = {{"number", no_argument, NULL, 'n'},
-                                  {"number-nonblank", no_argument, NULL, 'b'},
-                                  {"squeeze-blank", no_argument, NULL, 's'},
-                                  {"show-ends", no_argument, NULL, 'E'},
-                                  {"show-tabs", no_argument, NULL, 'T'},
-                                  {"show-nonprinting", no_argument, NULL, 'v'},
-                                  {0, 0, 0, 0}};
+  flags.pattern = malloc(MAX_PATTERN_SIZE * sizeof(char));
+  if (flags.pattern == NULL) {
+    perror("malloc");
+  }
   int opt;
-  while ((opt = getopt_long(argc, argv, "+bEnsetTv", long_options, NULL)) !=
-         -1) {
+  while ((opt = getopt(argc, argv, "e:ivclnhsf:o")) != -1) {
     switch (opt) {
-      case 'b':
-        flags.b = 1;
-        break;
-      case 'E':
-        flags.E = 1;
-        break;
-      case 'n':
-        flags.n = 1;
-        break;
-      case 's':
-        flags.s = 1;
-        break;
-      case 't':
-        flags.T = 1;
-        flags.v = 1;
-        break;
       case 'e':
-        flags.E = 1;
-        flags.v = 1;
+        flags.e = 1;
+        pattern_add(&flags, optarg);
         break;
-      case 'T':
-        flags.T = 1;
+      case 'i':
+        flags.i = REG_ICASE;
         break;
       case 'v':
         flags.v = 1;
         break;
+      case 'c':
+        flags.c = 1;
+        break;
+      case 'l':
+        flags.l = 1;
+        break;
+      case 'n':
+        flags.n = 1;
+        break;
+      case 'h':
+        flags.h = 1;
+        break;
+      case 's':
+        flags.s = 1;
+        break;
+      case 'f':
+        flags.f = 1;
+        add_reg_from_file(&flags, optarg);
+        break;
+      case 'o':
+        flags.o = 1;
+        break;
       default:
-        printf("Usage: 21_cat [-bEnsTv] [file..]\n");
-        *error_flag = 1;
+        printf("Usage: 21_grep [-eivclnhsfo] [file..]\n");
         break;
     }
+  }
+  if (flags.len_pattern == 0) {
+    pattern_add(&flags, argv[optind]);
+    optind++;
+  }
+  if (argc - optind == 1) {
+    flags.h = 1;
   }
   return flags;
 }
